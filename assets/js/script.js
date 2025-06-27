07a59174bd6a5b0b287cd93fe3ea364fea768aab
annesha_nisha <94369767+AnneshaAhsan@users.noreply.github.com>
2025-06-27 19:21:22 +0600

    Shop Section Addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,3 +63,19 @@ const headerSticky = function () {
 };
 
 addEventOnElem(window, "scroll", headerSticky);
+
+/* */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sections[i].classList.add("active");
+    }
+  }
+};
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
